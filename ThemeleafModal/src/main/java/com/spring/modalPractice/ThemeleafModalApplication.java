5c522f9a12015813d0dde111a4fef64358dfe5e9
raihanuddin561 <raihanuddin561@gmail.com>
2020-01-14 22:58:08 +0600

    modal practice using boot

---
+++ ThemeleafModal/src/main/java/com/spring/modalPractice/ThemeleafModalApplication.java
@@ -0,0 +1,13 @@
+package com.spring.modalPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemeleafModalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemeleafModalApplication.class, args);
+	}
+
+}
---
+++ ThemeleafModal/src/test/java/com/spring/modalPractice/ThemeleafModalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.modalPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemeleafModalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
